253989d49153de9a800581c9526fcc15c49c751f
Tony Hart <tonyzoof@gmail.com>
2022-03-22 16:02:49 -0400

    first commit

---
+++ database/migrations/2022_03_22_191806_create_translations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->id();
+            $table->string('source')->index('source_index');
+            $table->string('target')->index('target_index');
+            $table->text('text');
+            $table->text('translatedText');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,103 @@
+<?php
+
+use App\Http\Resources\TranslationResource;
+use App\Models\Translation;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('cache.headers:public;max_age=2628000;etag')->group(function () {
+    Route::post('/translate', function (Request $request) {
+        [, $query] = explode('?', $request->fullUrl() . '?');
+        $params = [];
+        foreach (explode('&', $request->getContent()) ?? [] as $param) {
+            [$key, $value] = explode('=', $param . '=');
+            if ($key === 'q') {
+                $params[] = urldecode($value);
+            }
+        }
+
+        if (count($params) === 0) {
+            return [
+                'data' => [
+                    'translations' => []
+                ]
+            ];
+        }
+
+        $cachedTranslations = Translation::query()
+            ->whereIn('text', $params)
+            ->get();
+
+        $missingTranslations = [];
+
+        foreach ($params as $q) {
+            $hasCachedTranslation = $cachedTranslations->where('text', '=', $q)->first();
+
+            if (!$hasCachedTranslation) {
+                $missingTranslations[] = $q;
+            }
+        }
+
+        if (count($missingTranslations) > 0) {
+            $trResult = Http::post("https://translation.googleapis.com/language/translate/v2?$query", [
+                'q' => $missingTranslations
+            ])->json('data.translations');
+
+            $saveTranslations = [];
+
+            foreach ($trResult as $key => $translation) {
+                if (isset($missingTranslations[$key], $translation['translatedText'])) {
+                    $saveTranslations[] = [
+                        'source' => $request->get('source'),
+                        'target' => $request->get('target'),
+                        'text' => $missingTranslations[$key],
+                        'translatedText' => $translation['translatedText'],
+                        'created_at' => now()->format('Y-m-d H:i:s'),
+                        'updated_at' => now()->format('Y-m-d H:i:s')
+                    ];
+                }
+            }
+
+            if (count($saveTranslations) > 0) {
+                Translation::query()->insert($saveTranslations);
+            }
+
+            $cachedTranslations = $cachedTranslations->merge(
+                Translation::query()->whereIn('text', $missingTranslations)->get()
+            );
+        }
+
+        $translations = [];
+
+        foreach ($params as $q) {
+            $hasCachedTranslation = $cachedTranslations->where('text', '=', $q)->first();
+            $translations[] = [
+                'translatedText' => optional($hasCachedTranslation)->translatedText,
+                'origin' => in_array($q, $missingTranslations) ? 'google' : 'cache'
+            ];
+        }
+
+
+        return [
+            'data' => [
+                'translations' => $translations
+            ]
+        ];
+    });
+
+    Route::get('translations/cached', function () {
+        return TranslationResource::collection(Translation::query()->latest()->limit(20)->get());
+    });
+});
